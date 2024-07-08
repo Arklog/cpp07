abde0a5ef57eb8831879efc8f61566c478889f4c
Pierre DUCLOUX <pducloux@made-f0Cr6s3.clusters.42paris.fr>
2024-07-08 17:37:53 +0200

    update

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,6 +3,30 @@
 //
 
 #include "Array.hpp"
+#include <iostream>
+
+template<typename T>
+void printArr(const Array<T> &arr) {
+    for (size_t i = 0; i < arr.size(); ++i)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+}
 
 int main()
-{}
+{
+    Array<int> arr(5);
+
+    printArr(arr);
+
+    for (int i = 0; i < 10; ++i) {
+        try {
+            arr[i] = i;
+        } catch (const std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
+
+    printArr(arr);
+
+    return (0);
+}
